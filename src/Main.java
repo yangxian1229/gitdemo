285fa7cc8903136b49f657192884a2df800b879c
yangxian007 <yangxian007@ke.com>
2019-07-29 18:21:03 +0800

    version-2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("version-2");
+        System.out.println("modify : version-2");
     }
 }
